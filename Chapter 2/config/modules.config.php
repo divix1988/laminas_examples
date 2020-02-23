adf3346344cfeca4000ab06e82255eae9354221b
Adam Omelak <divix1988@gmail.com>
2020-02-24 00:13:55 +0100

    Initial Chapter 2 code.

---
+++ Chapter 2/config/modules.config.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-mvc-skeleton for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mvc-skeleton/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mvc-skeleton/blob/master/LICENSE.md New BSD License
+ */
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Di',
+    'Laminas\Log',
+    'Laminas\Db',
+    'Laminas\Mvc\Plugin\FilePrg',
+    'Laminas\Mvc\Plugin\FlashMessenger',
+    'Laminas\Mvc\Plugin\Identity',
+    'Laminas\Mvc\Plugin\Prg',
+    'Laminas\Session',
+    'Laminas\Mvc\I18n',
+    'Laminas\Mvc\Console',
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\I18n',
+    'Laminas\Cache',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Laminas\DeveloperTools',
+    'Application',
+];
